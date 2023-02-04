766f879252019ed583062d814e3aa4da8835e4d0
Henry Le <henryle913@outlook.com>
2023-02-04 09:19:57 -0600

    first commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1654577.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 120384246, 120384247, 120384248, 120384249 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/120384270/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/120384271/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/120384269/1/ammo.js', 'preload' : true},
+];
